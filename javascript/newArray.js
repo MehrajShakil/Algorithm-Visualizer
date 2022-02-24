f9c083bfc89f1969ef15eda0ab1c2ce977c2f75a
Md. Mehraj Hossain <mdmehrajshakil98@gmail.com>
2022-02-24 22:16:40 +0600

    added

--- javascript/newArray.js
+++ javascript/newArray.js
@@ -1,7 +1,16 @@
 
-function generateArray(){
 let arr = [];
 let n = 20;
+
+function get(index){
+    index = "._"+index;
+    return index;
+}
+
+
+//  Array Generation
+
+function generateArray(){
 for( let i = 1 ; i<=n ; ++i ) {
      let x = Math.floor(Math.random()*20); 
      if(x<2) x = 2;
@@ -21,3 +30,48 @@ for ( let i = 1 ; i<=n ; ++i ) {
     document.querySelector(s).style.height = x;
 }
 }
+
+// Bubble sort
+
+function BubbleSort(){
+    
+    let brr = [];
+
+    for ( let i = 1 ; i<=n ; ++i ) brr[i] = arr[i];
+
+    let id = null;
+    
+    for(let i = 1; i<=n; i++){
+      for(let j = 1; j<=n-i; j++){
+        if(brr[j] > brr[j+1]){
+          let element1 = document.querySelector(get(j));
+          let element2 = document.querySelector(get(j+1));
+
+          let pos = 0;
+          
+          clearInterval(id);
+          id = setInterval(frame, 100);
+
+          element1.style.backgroundColor = "red";
+          element2.style.backgroundColor = "red";  
+
+          function frame() {
+            
+            if (pos >= 1.9) {
+              clearInterval(id);
+            } else {
+              pos+=0.1;
+              element1.style.left = pos + "rem";
+              element2.style.right = pos + 'rem';
+            }
+          }
+
+          element1.style.backgroundColor = "white";
+          element2.style.backgroundColor = "white";
+          let temp = brr[j];
+          brr[j] = brr[j + 1];
+          brr[j+1] = temp;
+        }
+      }
+    }
+   }
